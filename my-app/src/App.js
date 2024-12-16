d436e920d1b3e3b65340b531ef8c9e12ccbcd6db
Yulia-As-jpg <yulkahitrova26@gmail.com>
2024-12-16 14:59:00 +0300

    react

---
+++ my-app/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>TODO</h1>
+    </div>
+  );
+}
+
+
+const Task = () => {
+    return (
+        <div>
+            <input type="text" placeholder="What needs to be done?"></input>
+        </div>
+    )
+}
+
+
+
+export default App;
